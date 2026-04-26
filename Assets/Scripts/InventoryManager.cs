69a3228008b44586c23dd68ae8a7a9aa9dadc3d0
laskalissalizaxxx-glitch <laskalissalizaxxx@gmail.com>
2026-04-26 15:35:19 +0300

    Create InventoryManager.cs

---
+++ Assets/Scripts/InventoryManager.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// Управляет предметами в инвентаре игрока.
+/// </summary>
+public class InventoryManager : MonoBehaviour
+{
+    /// <summary>
+    /// Добавляет предмет в коллекцию игрока.
+    /// </summary>
+    /// <param name="itemName">Имя предмета для добавления.</param>
+    public void AddItem(string itemName) { }
+
+    /// <summary>
+    /// Проверяет, есть ли предмет в инвентаре.
+    /// </summary>
+    /// <returns>True, если предмет найден, иначе false.</returns>
+    public bool HasItem(string itemName) { return false; }
+}
